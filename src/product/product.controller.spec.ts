ec5fa4324f48959376c2e69b3aca73c9191a6269
Emiliano Salazar <salazaremiliano84@gmail.com>
2023-12-12 09:35:16 -0300

    sumando testeos al recurso

--- src/product/product.controller.spec.ts
+++ src/product/product.controller.spec.ts
@@ -4,6 +4,7 @@ import { ProductService } from './product.service';
 
 describe('ProductController', () => {
   let controller: ProductController;
+  let service: ProductService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -12,9 +13,19 @@ describe('ProductController', () => {
     }).compile();
 
     controller = module.get<ProductController>(ProductController);
+    service = module.get<ProductService>(ProductService)
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
   });
+
+  it('should delete a product of array', () => {
+    expect(controller.remove('5')).not.toBeNull()
+  })
+
+  it('should get a product by id',()=>{
+    
+
+  })
 });
